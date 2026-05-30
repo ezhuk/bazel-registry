2dead12e4c78e703d7c611eb69cad0aa108ec10a
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-05-30 21:48:31 +0000

    Add portability scaffold

--- tests/folly/main.cpp
+++ tests/folly/main.cpp
@@ -1,6 +1,19 @@
-#include <folly/CPortability.h>
-#include <folly/Portability.h>
+#include <folly/folly-config.h>
+
+#include <iostream>
 
 int main() {
+#ifdef FOLLY_HAVE_PTHREAD
+  std::cout << "FOLLY_HAVE_PTHREAD=1\n";
+#else
+  std::cout << "FOLLY_HAVE_PTHREAD=0\n";
+#endif
+
+#ifdef FOLLY_HAVE_LIBGLOG
+  std::cout << "FOLLY_HAVE_LIBGLOG=1\n";
+#else
+  std::cout << "FOLLY_HAVE_LIBGLOG=0\n";
+#endif
+
   return 0;
 }
