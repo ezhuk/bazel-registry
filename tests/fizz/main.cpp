ba6cbe5e509892bd818f68f4561fb751407c5c9e
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-06-13 13:45:47 -0700

    Add fizz compat

--- tests/fizz/main.cpp
+++ tests/fizz/main.cpp
@@ -1,7 +1,6 @@
-#include <fizz/backend/openssl/certificate/X509ChainWithPkey.h>
 #include <fizz/record/Types.h>
-#include <fizz/server/DefaultCertManager.h>
 
 int main() {
-  return 0;
+  auto alert = fizz::AlertDescription::close_notify;
+  return alert == fizz::AlertDescription::close_notify ? 0 : 1;
 }
