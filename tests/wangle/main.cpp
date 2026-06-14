daa2fe6504b81d45694a8c2ffe66dbb440d549d6
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-06-14 15:58:02 -0700

    Add wangle test

--- tests/wangle/main.cpp
+++ tests/wangle/main.cpp
@@ -1,5 +1,12 @@
+#include <folly/io/IOBufQueue.h>
 #include <wangle/bootstrap/ServerBootstrap.h>
 #include <wangle/channel/Pipeline.h>
+
+#include <string>
+
+struct Message {
+  std::string data;
+};
 
 using Pipeline = wangle::Pipeline<folly::IOBufQueue&, Message>;
 
